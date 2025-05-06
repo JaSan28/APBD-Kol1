d36c6a566134674d62c039440382f84d36f74851
Jakub <s28140@pjwstk.edu.pl>
2025-05-07 00:46:48 +0200

    szkielet

--- Kolokwium/Kolokwium/Controllers/Controller.cs
+++ Kolokwium/Kolokwium/Controllers/Controller.cs
@@ -1,6 +1,24 @@
 ﻿namespace Kolokwium.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
-public class Controller
-{
+
+[Route("api/[controller]")]
+    [ApiController]
+	public class Controller
+	{
     
-}
+		//[HttpGet]
+		//	public async Task<IActionResult> nazwamet()
+		//	{
+				
+		//	}
+		
+		//[HttpGet("{id}/trips")]
+		//	public async Task<IActionResult> nazwame(typ nazwazm)
+		
+		
+	
+	
+	
+	}
--- Kolokwium/Kolokwium/Services/Service.cs
+++ Kolokwium/Kolokwium/Services/Service.cs
@@ -2,5 +2,5 @@
 
 public class Service
 {
-    
+    private readonly string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;";
 }
